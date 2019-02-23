1077971a00bdcdef4e18e89c976f3b4f65d84570
lourod1987 <louart87@gmail.com>
2019-02-22 21:57:40 -0600

    creating skiDay reducer and changing code to test it in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import C from './constants';
-import { goal } from './store/reducers'
+import { skiDay } from './store/reducers'
 // import { allSkiDays, goal } from './initialState.json';
 
 // console.log(`
@@ -13,17 +13,27 @@ import { goal } from './store/reducers'
 //     ${Object.keys(C).join('\n     ')}
 // `);
 
-const state = 10;
+const state = null;
+
+// const action = {
+//     type: C.SET_GOAL,
+//     payload: 15
+// }
 
 const action = {
-    type: C.SET_GOAL,
-    payload: 15
+    type: C.ADD_DAY,
+    payload: {
+        "resort": "Heavenly",
+        "date": "2016-12-16",
+        "powder": true,
+        "backcountry": false
+    }
 }
 
-const nextState = goal(state, action);
+const nextState = skiDay(state, action);
 
 console.log(`
-    initial goal: ${state}
+    initial state: ${state}
     action: ${JSON.stringify(action)}
-    new goal: ${nextState}
+    new state: ${JSON.stringify(nextState)}
 `)
--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,9 +1,11 @@
 import C from '../constants';
 
-export const goal = (state, action) => {
-    if (action.type === C.SET_GOAL) {
-        return parseInt(action.payload);
-    } else {
-        return state;
-    }
-}
+export const goal = (state, action) => 
+    (action.type === C.SET_GOAL) ?
+        parseInt(action.payload) :
+        state
+
+export const skiDay = (state=null, action) =>
+    (action.type === C.ADD_DAY) ?
+        action.payload:
+        state
