e09312dc846837f90c3b7eec2c5946377fc87838
lourod1987 <louart87@gmail.com>
2019-02-28 23:43:20 -0600

    adding in errors and allSkiDays reducer functions

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,25 +1,38 @@
+/* eslint-disable indent */
 import C from '../constants';
 
 export const goal = (state, action) => 
-    (action.type === C.SET_GOAL) ?
-        parseInt(action.payload) :
-        state
+  (action.type === C.SET_GOAL) ?
+    parseInt(action.payload) :
+    state;
 
 export const skiDay = (state=null, action) =>
-    (action.type === C.ADD_DAY) ?
-        action.payload:
-        state
+  (action.type === C.ADD_DAY) ?
+    action.payload:
+    state;
 
 export const errors = (state=[], action) => {
-    switch(action.type) {
-        case C.ADD_ERROR:
-            return [
-                ...state,
-                action.payload
-            ]
-        case C.CLEAR_ERROR:
-            return state.filter((message, i) => i !== action.payload)
-        default:
-            return state;
-    }
-}
+  switch(action.type) {
+  case C.ADD_ERROR:
+    return [
+      ...state,
+      action.payload
+    ];
+  case C.CLEAR_ERROR:
+    return state.filter((message, i) => i !== action.payload);
+  default:
+    return state;
+  }
+};
+
+export const allSkiDays = (state=[], action) => {
+  switch(action.type) {
+    case C.ADD_DAY:
+      return [
+        ...state,
+        skiDay(null, action)
+      ];
+    default:
+      state;
+  }
+};
