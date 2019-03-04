3864646a496b0ea9d259ec921bba5bd58cbb7890
lourod1987 <louart87@gmail.com>
2019-03-04 02:14:25 -0600

    adding the change suggestions action.type to both the fetching and suggestions reducers to make sure all needed changes occur for the change suggestions action

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -53,6 +53,8 @@ export const fetching = (state=false, action) => {
       return true;
     case C.CANCEL_FETCHING:
       return false;
+    case C.CHANGE_SUGGESTIONS:
+      return false;
     default:
       return state;
   }
@@ -62,6 +64,8 @@ export const suggestions = (state=[], action) => {
   switch(action.type) {
     case C.CLEAR_SUGGESTIONS:
       return [];
+    case C.CHANGE_SUGGESTIONS:
+      return action.payload;
     default:
       return state;
   }
