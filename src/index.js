f58f116d5f04b559660fe555375cb5dfd93823b6
lourod1987 <louart87@gmail.com>
2019-03-08 00:17:09 -0600

    setting dispatches to put the middleware created to use

--- src/index.js
+++ src/index.js
@@ -1,26 +1,85 @@
 /* eslint-disable no-console */
 /* eslint-disable quotes */
 import C from './constants';
-import appReducer from './store/reducers';
+import storeFactory from './store';
+import { addDay } from './actions';
+// import appReducer from './store/reducers';
 // import initialState from './initialState.json';
-import { createStore } from 'redux';
+// import { createStore } from 'redux';
 
-const initialState = (localStorage['redux-store']) ? 
+const initialState = (localStorage['redux-store']) ?
   JSON.parse(localStorage['redux-store']) :
   {};
 
+const saveState = () => {
+  const state = JSON.stringify(store.getState());
+  localStorage['redux-store'] = state;
+};
+
+const store = storeFactory(initialState);
+
+store.subscribe(saveState);
+
+store.dispatch({
+  type: C.ADD_DAY,
+  payload: {
+    'resort': 'Mt Shasta',
+    'date': '2016-10-28',
+    'powder': true,
+    'backcountry': true
+  }
+});
+
+store.dispatch({
+  type: C.ADD_DAY,
+  payload: {
+    'resort': 'Squaw Valley',
+    'date': '2016-3-28',
+    'powder': true,
+    'backcountry': false
+  }
+});
+
+store.dispatch({
+  type: C.ADD_DAY,
+  payload: {
+    'resort': 'The Canyons',
+    'date': '2016-1-2',
+    'powder': false,
+    'backcountry': true
+  }
+});
+
+// const initialState = (localStorage['redux-store']) ? 
+//   JSON.parse(localStorage['redux-store']) :
+//   {};
+
+// const store = createStore(appReducer);
+
+// const unsubscribeGoalLogger = store.subscribe(() => console.log(`    Goal: ${store.getState().goal}`));
+
+// setInterval(() => {
+//   store.dispatch({
+//     type: C.SET_GOAL,
+//     payload: Math.floor(Math.random() * 100)
+//   });
+// }, 250);
+
+// setTimeout(() => {
+//   unsubscribeGoalLogger();
+// }, 3000);
+
 // const store = createStore(appReducer, initialState);
-const store = createStore(appReducer, initialState);
 
 // store.subscribe( () => console.log(store.getState()));
 
-store.subscribe( () => {
-  const state = JSON.stringify(store.getState());
-  localStorage['redux-store'] = state;
-});
+// store.subscribe( () => {
+//   const state = JSON.stringify(store.getState());
+//   localStorage['redux-store'] = state;
+// });
 
 
-window.store = store;
+// window.store = store;
 // store.dispatch({
 //   type: C.ADD_DAY,
 //   payload: {
