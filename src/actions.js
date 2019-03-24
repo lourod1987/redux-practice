09694ea0d46e6f7a2aa78ea0fbb327eb218ebf8a
lourod1987 <louart87@gmail.com>
2019-03-24 16:40:16 -0500

    action creator functions for addError, clearError, changeSuggestions, clearSuggestions, and randomGoals

--- src/actions.js
+++ src/actions.js
@@ -1,4 +1,5 @@
 import C from './constants';
+import { suggestions } from './store/reducers';
 
 export function addDay(resort, date, powder=false, backcountry=false) {
   //Add app logic here... if needed
@@ -19,3 +20,37 @@ export const setGoal = (goal) => ({
   type: C.SET_GOAL,
   payload: goal
 });
+
+export const addError = (message) => ({
+  type: C.ADD_ERROR,
+  payload: message
+});
+
+export const clearError = (index) => ({
+  type: C.CLEAR_ERROR,
+  payload: index
+});
+
+export const changeSuggestions = suggestions => ({
+  type: C.CHANGE_SUGGESTIONS,
+  payload: suggestions
+});
+
+export const clearSuggestions = () => ({
+  type: C.CLEAR_SUGGESTIONS
+});
+
+export const randomGoals = () => (dispatch, getState) => {
+
+  if (!getState().resortNames.fetching) {
+    dispatch({
+      type: C.FETCH_RESORT_NAMES
+    });
+    
+    setTimeout(() =>{
+      dispatch({
+        type: C.CANCEL_FETCHING
+      });
+    }, 1500);
+  }
+};
