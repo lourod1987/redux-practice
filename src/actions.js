5256b3a314ca320e37b617e7a718c5939509924c
lourod1987 <louart87@gmail.com>
2019-03-25 12:06:21 -0500

    action creator function for suggestResortNames.

--- src/actions.js
+++ src/actions.js
@@ -1,5 +1,6 @@
 import C from './constants';
 import { suggestions } from './store/reducers';
+import fetch from 'isomorphic-fetch';
 
 export function addDay(resort, date, powder=false, backcountry=false) {
   //Add app logic here... if needed
@@ -53,4 +54,27 @@ export const randomGoals = () => (dispatch, getState) => {
       });
     }, 1500);
   }
+};
+
+export const suggestResortNames = value => dispatch => {
+  dispatch({
+    type: C.FETCH_RESORT_NAMES
+  });
+
+  fetch('http://localhost:3333/resorts/' + value)
+    .then(response => response.json())
+    .then(suggestions => {
+      dispatch({
+        type: C.CHANGE_SUGGESTIONS,
+        payload: suggestions
+      });
+    })
+    .catch(error => {
+      dispatch(
+        addError(error.message)
+      );
+      dispatch({
+        type: C.CANCEL_FETCHING
+      });
+    });
 };
