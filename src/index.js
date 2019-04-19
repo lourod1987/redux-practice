4ef3f9c3f8fde7f827f26ec46781f4ac0734250a
lourod1987 <louart87@gmail.com>
2019-04-18 22:37:04 -0500

    Refactor - removing commented out code. Adding in component for interfacing react with redux. Error handling and store instantiated.

--- src/index.js
+++ src/index.js
@@ -5,14 +5,9 @@ import React from 'react';
 import { render } from 'react-dom';
 import routes from './routes';
 import sampleData from './initialState';
-// import storeFactory from './store';
-// import { addDay, removeDay, setGoal,
-//   addError, clearError, changeSuggestions, clearSuggestions, randomGoals, suggestResortNames
-// } from './actions';
-// import appReducer from './store/reducers';
-// import initialState from './initialState.json';
-// import { createStore } from 'redux';
-
+import storeFactory from './store';
+import { Provider } from 'react-redux';
+import { addError } from './actions';
 
 const initialState = (localStorage["redux-store"]) ?
   JSON.parse(localStorage["redux-store"]) :
@@ -21,135 +16,26 @@ const initialState = (localStorage["redux-store"]) ?
 const saveState = () => 
   localStorage["redux-store"] = JSON.stringify(store.getState());
 
+const handleError = error => {
+  store.dispatch(
+    addError(error.message)
+  );
+};
+
+const store = storeFactory(initialState);
+store.subscribe(saveState);
+
 window.React = React;
+window.store = store;
+
+window.addEventListener("error", handleError);
 
 render(
-  routes,
+  <Provider store={store}>
+    {routes}
+  </Provider>,
   document.getElementById('react-container')
 );
 
-// const store = storeFactory();
 
-// store.dispatch(
-//   suggestResortNames("hea")
-// );
-
-// store.dispatch(
-//   addDay("Heavenly", "2016-12-22")
-// );
-
-// store.dispatch(
-//   removeDay("2016-12-22")
-// );
-
-// store.dispatch(
-//   setGoal(55)
-// );
-
-// store.dispatch(
-//   addError("something went wrong")
-// );
-
-// store.dispatch(
-//   clearError(0)
-// );
-
-// store.dispatch(
-//   changeSuggestions(['One', 'Two', 'Three'])
-// );
-
-// store.dispatch(
-//   clearSuggestions()
-// );
-
-// store.dispatch(
-//   randomGoals()
-// );
-
-// const initialState = (localStorage['redux-store']) ?
-//   JSON.parse(localStorage['redux-store']) :
-//   {};
-
-// const saveState = () => {
-//   const state = JSON.stringify(store.getState());
-//   localStorage['redux-store'] = state;
-// };
-
-// const store = storeFactory(initialState);
-
-// store.subscribe(saveState);
-
-// store.dispatch({
-//   type: C.ADD_DAY,
-//   payload: {
-//     'resort': 'Mt Shasta',
-//     'date': '2016-10-28',
-//     'powder': true,
-//     'backcountry': true
-//   }
-// });
-
-// store.dispatch({
-//   type: C.ADD_DAY,
-//   payload: {
-//     'resort': 'Squaw Valley',
-//     'date': '2016-3-28',
-//     'powder': true,
-//     'backcountry': false
-//   }
-// });
-
-// store.dispatch({
-//   type: C.ADD_DAY,
-//   payload: {
-//     'resort': 'The Canyons',
-//     'date': '2016-1-2',
-//     'powder': false,
-//     'backcountry': true
-//   }
-// });
-
-// const initialState = (localStorage['redux-store']) ? 
-//   JSON.parse(localStorage['redux-store']) :
-//   {};
-
-// const store = createStore(appReducer);
-
-// const unsubscribeGoalLogger = store.subscribe(() => console.log(`    Goal: ${store.getState().goal}`));
-
-// setInterval(() => {
-//   store.dispatch({
-//     type: C.SET_GOAL,
-//     payload: Math.floor(Math.random() * 100)
-//   });
-// }, 250);
-
-// setTimeout(() => {
-//   unsubscribeGoalLogger();
-// }, 3000);
-
-// const store = createStore(appReducer, initialState);
-
-// store.subscribe( () => console.log(store.getState()));
-
-// store.subscribe( () => {
-//   const state = JSON.stringify(store.getState());
-//   localStorage['redux-store'] = state;
-// });
-
-
-// window.store = store;
-// store.dispatch({
-//   type: C.ADD_DAY,
-//   payload: {
-//     'resort': 'Mt Shasta',
-//     'date': '2016-10-28',
-//     'powder': false,
-//     'backcountry': true
-//   }
-// });
-
-// store.dispatch({
-//   type: C.SET_GOAL,
-//   payload: 2
-// });
+foo = bar;
